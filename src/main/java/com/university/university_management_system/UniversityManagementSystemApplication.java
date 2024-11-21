f1d15e627aaeaa68568d7b58d40927056f254fd3
Abdul Basit <abdlbasit003@gmail.com>
2024-11-21 20:03:32 +0500

    test push 2

--- src/main/java/com/university/university_management_system/UniversityManagementSystemApplication.java
+++ src/main/java/com/university/university_management_system/UniversityManagementSystemApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UniversityManagementSystemApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Testing push");
+		System.out.println("Testing push 2");
 		SpringApplication.run(UniversityManagementSystemApplication.class, args);
 	}
 
