23554b6c2dbe3574fa42834b37a3cce87d9f135b
Abdul Basit <basitirfan760@gmail.com>
2024-11-21 18:28:49 +0500

    Skeleton project

---
+++ src/test/java/com/university/university_management_system/UniversityManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.university.university_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
