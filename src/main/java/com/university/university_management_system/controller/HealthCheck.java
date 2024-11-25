f40b16a5fbe7683ac0f63af2762cfe3f48babf63
Abdul Basit <abdlbasit003@gmail.com>
2024-11-25 21:33:50 +0500

    initial connection established
    App is up and running, with a test endpoint named healthCheck
    Hit localhost:8080/ok to verify

---
+++ src/main/java/com/university/university_management_system/controller/HealthCheck.java
@@ -0,0 +1,12 @@
+package com.university.university_management_system.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheck {
+    @GetMapping("/ok")
+    public String healthCheck(){
+        return "OK";
+    }
+}
