1cf052145072be4ec2639cb38d583ab82f23abee
Maheen Fatima <s.maheenfatima16@gmail.com>
2024-12-03 23:32:51 +0500

    added annotations

--- src/main/java/com/university/university_management_system/controller/CourseInstructorController.java
+++ src/main/java/com/university/university_management_system/controller/CourseInstructorController.java
@@ -3,17 +3,24 @@ package com.university.university_management_system.controller;
 import com.university.university_management_system.model.CourseInstructorModel;
 import com.university.university_management_system.repository.CourseInstructorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
+@RequestMapping("/courseinstructors")
 public class CourseInstructorController {
     @Autowired
     CourseInstructorRepository courseInstructorRepository;
-
+    @GetMapping("")
     public List<CourseInstructorModel> getAllCourseInstructor(){
         return courseInstructorRepository.findAll();
     }
-    public CourseInstructorModel getCourseInstructorbyID(int id){
-        return courseInstructorRepository.findById(id).orElseThrow();
+    @GetMapping("/{courseInstructorId}")
+    public CourseInstructorModel getCourseInstructorbyID(@PathVariable int courseInstructorId){
+        return courseInstructorRepository.findById(courseInstructorId).orElseThrow();
     }
 }
