b0550ce6033f56ae92b24c2b94471337374b5f96
Abdullah <dojkyabdullah@gmail.com>
2024-12-03 15:58:43 +0500

    Course Result Controller class methods created with mappings

---
+++ src/main/java/com/university/university_management_system/controller/CourseResultController.java
@@ -0,0 +1,25 @@
+package com.university.university_management_system.controller;
+
+import com.university.university_management_system.model.CourseResultModel;
+import com.university.university_management_system.repository.CourseResultRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CourseResultController {
+    @Autowired
+    CourseResultRepository courseResultRepository;
+
+    @GetMapping("/courseResults")
+    public List<CourseResultModel> getAllCourseResults() {
+        return courseResultRepository.findAll();
+    }
+    @GetMapping("/courseResults/{courseResultId}")
+    public CourseResultModel getCourseResultById(@PathVariable int courseResultId) {
+        return courseResultRepository.findById(courseResultId).orElseThrow();
+    }
+}
