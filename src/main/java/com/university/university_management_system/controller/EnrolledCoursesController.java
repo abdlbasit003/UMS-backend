b745989a4eb101379ac2ae0d8f62f13a92b94051
Maheen Fatima <s.maheenfatima16@gmail.com>
2024-12-03 14:13:43 +0500

    added spring annotations

--- src/main/java/com/university/university_management_system/controller/EnrolledCoursesController.java
+++ src/main/java/com/university/university_management_system/controller/EnrolledCoursesController.java
@@ -3,20 +3,28 @@ package com.university.university_management_system.controller;
 import com.university.university_management_system.model.EnrolledCourseModel;
 import com.university.university_management_system.repository.EnrolledCourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
 
+@RestController
+@RequestMapping("/enrolledcourses")
 public class EnrolledCoursesController {
     @Autowired
     EnrolledCourseRepository enrolledCourseRepository;
 
 
+    @GetMapping("")
     public List<EnrolledCourseModel> getAllEnrolledCourses(){
         return enrolledCourseRepository.findAll();
     }
 
-    public EnrolledCourseModel getEnrolledCourseByID(Integer id){
+    @GetMapping("/{id}")
+    public EnrolledCourseModel getEnrolledCourseByID(@PathVariable int id){
         return enrolledCourseRepository.findById(id).orElseThrow();
     }
 
