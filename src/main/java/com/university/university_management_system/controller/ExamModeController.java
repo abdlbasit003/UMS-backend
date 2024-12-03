ae10ad338bd8485ce503ef0f1367fd481821878b
Abdul Basit <abdlbasit003@gmail.com>
2024-12-03 19:09:34 +0500

    Annotated Exam Mode controller

--- src/main/java/com/university/university_management_system/controller/ExamModeController.java
+++ src/main/java/com/university/university_management_system/controller/ExamModeController.java
@@ -3,14 +3,22 @@ package com.university.university_management_system.controller;
 import com.university.university_management_system.model.ExamModeModel;
 import com.university.university_management_system.repository.ExamModeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
+@RequestMapping("/exammodes")
 public class ExamModeController {
     @Autowired
-    ExamModeRepository repository;
+    ExamModeRepository examModeRepository;
 
-    public List<ExamModeModel> getAllExamTypes() {return repository.findAll();}
+    @GetMapping("")
+    public List<ExamModeModel> getAllExamTypes() {return examModeRepository.findAll();}
 
-    public ExamModeModel getExamTypeById(int id) {return repository.findById(id).orElseThrow();}
+    @GetMapping("/{examModeId}")
+    public ExamModeModel getExamTypeById(@PathVariable int examModeId) {return examModeRepository.findById(examModeId).orElseThrow();}
 }
