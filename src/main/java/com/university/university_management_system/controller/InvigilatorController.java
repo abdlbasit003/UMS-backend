5c0ae8e79495e0a5ff38a7c347a0512cc7bdc3cf
Maheen Fatima <s.maheenfatima16@gmail.com>
2024-12-03 22:04:55 +0500

    Added annotations

--- src/main/java/com/university/university_management_system/controller/InvigilatorController.java
+++ src/main/java/com/university/university_management_system/controller/InvigilatorController.java
@@ -3,20 +3,27 @@ package com.university.university_management_system.controller;
 import com.university.university_management_system.model.InvigilatorModel;
 import com.university.university_management_system.repository.InvigilatorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@RestController
+@RequestMapping("/invigilators")
 public class InvigilatorController {
 
     @Autowired
     InvigilatorRepository invigilatorRepository;
 
+    @GetMapping("")
     public List<InvigilatorModel> GetAllInvigilator() {
         return invigilatorRepository.findAll();
     }
 
-    public InvigilatorModel GetInvigilatorbyid(int id) {
-        return invigilatorRepository.findById(id).orElseThrow();
+    @GetMapping("/{invigilatorId}")
+    public InvigilatorModel GetInvigilatorbyid(@PathVariable int invigilatorId) {
+        return invigilatorRepository.findById(invigilatorId).orElseThrow();
 
     }
 }
