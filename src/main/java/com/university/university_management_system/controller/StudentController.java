4f82f0a3c8bcdd30ad691d5207d1cd843cb8d2e1
Abdul Basit <abdlbasit003@gmail.com>
2024-12-03 18:58:02 +0500

    Annotated student controller

--- src/main/java/com/university/university_management_system/controller/StudentController.java
+++ src/main/java/com/university/university_management_system/controller/StudentController.java
@@ -3,19 +3,26 @@ package com.university.university_management_system.controller;
 import com.university.university_management_system.model.StudentModel;
 import com.university.university_management_system.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
+@RequestMapping("/students")
 public class StudentController {
     @Autowired
     StudentRepository studentRepository;
 
-
+    @GetMapping("")
     public List<StudentModel> getAllStudents(){
         return studentRepository.findAll();
 
     }
-    public StudentModel getStudentById(String studentId){
+    @GetMapping("/{studentId}")
+    public StudentModel getStudentById(@PathVariable String studentId){
        return studentRepository.findById(studentId).orElseThrow();
 
     }
