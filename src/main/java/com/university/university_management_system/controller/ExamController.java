22f3d203a8df620f82e48886181fa61c1e881ac8
Abdul Basit <abdlbasit003@gmail.com>
2024-12-03 19:16:59 +0500

    Annotated Exam controller

--- src/main/java/com/university/university_management_system/controller/ExamController.java
+++ src/main/java/com/university/university_management_system/controller/ExamController.java
@@ -5,22 +5,23 @@ import com.university.university_management_system.repository.ExamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
-
+@RequestMapping("/exams")
 public class ExamController {
     @Autowired
     ExamRepository examRepository;
 
-    @GetMapping("/exams")
+    @GetMapping("")
     public List<ExamModel> getAllExams() {
         return examRepository.findAll();
     }
-    @GetMapping("/exams/{examId}")
+    @GetMapping("/{examId}")
     public ExamModel getExamById(@PathVariable int examId) {
         return examRepository.findById(examId).orElseThrow();
     }
