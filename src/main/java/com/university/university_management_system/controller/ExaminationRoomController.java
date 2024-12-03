8e01d5fea03643cba444e316b0fa45c6d398f2ea
Abdul Basit <abdlbasit003@gmail.com>
2024-12-03 19:41:43 +0500

    Annotated Examination controller

--- src/main/java/com/university/university_management_system/controller/ExaminationRoomController.java
+++ src/main/java/com/university/university_management_system/controller/ExaminationRoomController.java
@@ -5,18 +5,26 @@ import com.university.university_management_system.model.EnrolledCourseModel;
 import com.university.university_management_system.model.ExaminationRoomModel;
 import com.university.university_management_system.repository.ExaminationRoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
+@RequestMapping("/examrooms")
 public class ExaminationRoomController {
     @Autowired
     ExaminationRoomRepository examinationRoomRepository;
 
+    @GetMapping("")
     public List<ExaminationRoomModel> getAllExaminationRooms(){
         return examinationRoomRepository.findAll();
     }
 
-    public ExaminationRoomModel getExaminationRoomByID(Integer id){
-        return examinationRoomRepository.findById(id).orElseThrow();
+    @GetMapping("/{examRoomId}")
+    public ExaminationRoomModel getExaminationRoomByID(@PathVariable Integer examRoomId){
+        return examinationRoomRepository.findById(examRoomId).orElseThrow();
     }
 }
