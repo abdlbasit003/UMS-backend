972b6386b25cabb99ea8ae00ee55adbd41b64066
Abdullah <dojkyabdullah@gmail.com>
2024-12-16 20:24:14 +0500

    Ums-176 completed
    Created ExamDTO , ExamService and ExamController. Tested all the Api endpoints.

--- src/main/java/com/university/university_management_system/controller/ExamController.java
+++ src/main/java/com/university/university_management_system/controller/ExamController.java
@@ -5,31 +5,57 @@ import com.university.university_management_system.model.ExamModel;
 import com.university.university_management_system.repository.ExamRepository;
 import com.university.university_management_system.service.ExamService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 import java.util.Optional;
-
 @RestController
 @RequestMapping("/exams")
 public class ExamController {
+
     @Autowired
     ExamService examService;
 
     @GetMapping("")
-    public List<ExamDTO> getAllExams() {
-        return examService.getAllExams();
-    }
-    @GetMapping("/{examId}")
-    public ExamDTO getExamById(@PathVariable int examId) {
-        return examService.getExamById(examId);
+    public ResponseEntity<List<ExamDTO>> getAllExams() {
+        return ResponseEntity.ok(examService.getAllExams());
     }
 
-    @GetMapping("/{courseCode}")
-    public List<ExamDTO> getExamsByCourseCode(@PathVariable String courseCode) {
-        return examService.getExamsByCourseCode(courseCode);
+    @GetMapping("/id/{examId}")
+    public ResponseEntity<ExamDTO> getExamById(@PathVariable int examId) {
+        return ResponseEntity.ok(examService.getExamById(examId));
+    }
+
+    @GetMapping("/course/{courseCode}")
+    public ResponseEntity<List<ExamDTO>> getExamsByCourseCode(@PathVariable String courseCode) {
+        return ResponseEntity.ok(examService.getExamsByCourseCode(courseCode));
+    }
+
+    @GetMapping("/type/{examTypeId}")
+    public ResponseEntity<List<ExamDTO>> getExamsByExamTypeId(@PathVariable int examTypeId) {
+        return ResponseEntity.ok(examService.getExamsByExamTypeId(examTypeId));
+    }
+
+    @GetMapping("/hall/{examHallId}")
+    public ResponseEntity<List<ExamDTO>> getExamsByExamHallId(@PathVariable int examHallId) {
+        return ResponseEntity.ok(examService.getExamsByExamHallId(examHallId));
+    }
+
+    @GetMapping("/mode/{examModeId}")
+    public ResponseEntity<List<ExamDTO>> getExamsByExamModeId(@PathVariable int examModeId){
+        return ResponseEntity.ok(examService.getExamsByExamModeId(examModeId));
+    }
+
+    @GetMapping("/date/{examDate}")
+    public ResponseEntity<List<ExamDTO>> getExamsByDate(@PathVariable LocalDate examDate){
+        return ResponseEntity.ok(examService.getExamsByDate(examDate));
+    }
+
+    @GetMapping("/time")
+    public ResponseEntity<List<ExamDTO>> getExamsByTimeSlot(@RequestParam String examStartTime,@RequestParam String examEndTime){
+    return ResponseEntity.ok(examService.getExamsByTimeSlot(LocalTime.parse(examStartTime), LocalTime.parse(examEndTime)));
     }
 }
--- src/main/java/com/university/university_management_system/service/ExamService.java
+++ src/main/java/com/university/university_management_system/service/ExamService.java
@@ -1,14 +1,19 @@
 package com.university.university_management_system.service;
 
 import com.university.university_management_system.DTOs.ExamDTO;
+import com.university.university_management_system.exceptions.ApiException;
 import com.university.university_management_system.model.ExamModel;
+import com.university.university_management_system.model.ExamTypeModel;
 import com.university.university_management_system.repository.ExamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,8 +24,13 @@ public class ExamService {
 
 
     public List<ExamDTO> getAllExams(){
-        List<ExamDTO> allExamDTOs= new ArrayList<>();
+
         List<ExamModel> allExams = examRepository.findAll();
+
+        if (allExams.isEmpty()){
+            throw new ApiException("No exams Found", HttpStatus.NOT_FOUND );
+        }
+        List<ExamDTO> allExamDTOs= new ArrayList<>();
         for (ExamModel exam: allExams ){
             ExamDTO examDTO = ExamDTO.fromModel(exam);
             allExamDTOs.add(examDTO);
@@ -30,22 +40,88 @@ public class ExamService {
 
 
     public ExamDTO getExamById(int examId){
-        ExamModel examModel = examRepository.findById(examId).orElseThrow();
+
+        ExamModel examModel = examRepository.findById(examId).orElseThrow(()->new ApiException("No exam found for examId: "+ examId, HttpStatus.NOT_FOUND));
 
         return ExamDTO.fromModel(examModel);
 
     }
 
     public List<ExamDTO> getExamsByCourseCode(String courseCode) {
+
+        if (courseCode == null ){
+            throw new ApiException("No course Code Found", HttpStatus.NOT_FOUND);
+        }
         List<ExamModel> examsByCourse = examRepository.getExamsByCourseCode(courseCode);
         if (examsByCourse.isEmpty()) {
-            throw new IllegalArgumentException("No exams found for course code: " + courseCode);
+            throw new ApiException("No exams found for course code: " + courseCode, HttpStatus.NOT_FOUND);
         }
         List<ExamDTO> examDTOs = new ArrayList<>();
         for (ExamModel exam : examsByCourse) {
             examDTOs.add(ExamDTO.fromModel(exam));
         }
         return examDTOs;
+    }
+
+    public List<ExamDTO> getExamsByExamTypeId(int examTypeId){
+        List<ExamModel> examsByExamTypeId= examRepository.getExamsByExamTypeId(examTypeId);
+        if(examsByExamTypeId.isEmpty()){
+            throw new ApiException("No exams found for examTypeId: " + examTypeId, HttpStatus.NOT_FOUND);
+        }
+        List<ExamDTO> examDTOS = new ArrayList<>();
+        for(ExamModel exam: examsByExamTypeId){
+            examDTOS.add(ExamDTO.fromModel(exam));
+        }
+        return examDTOS;
+    }
+    public List<ExamDTO> getExamsByExamHallId(int examHallId){
+        List<ExamModel> examsByExamHallId= examRepository.getExamsByExamHallId(examHallId);
+        if(examsByExamHallId.isEmpty()){
+            throw new ApiException("No exams found for examHallId: " + examHallId, HttpStatus.NOT_FOUND);
+        }
+        List<ExamDTO> examDTOS = new ArrayList<>();
+        for(ExamModel exam: examsByExamHallId){
+            examDTOS.add(ExamDTO.fromModel(exam));
+        }
+        return examDTOS;
+    }
+    public List<ExamDTO> getExamsByExamModeId(int examModeId){
+        List<ExamModel> examsByExamModeId = examRepository.getExamsByExamModeId(examModeId);
+        if (examsByExamModeId.isEmpty()){
+            throw new ApiException("No exams found for examModeId: " + examModeId, HttpStatus.NOT_FOUND);
+        }
+        List<ExamDTO> examDTOS = new ArrayList<>();
+        for (ExamModel exam: examsByExamModeId){
+            examDTOS.add(ExamDTO.fromModel(exam));
+        }
+        return examDTOS;
+
+    }
+    public List<ExamDTO> getExamsByDate(LocalDate examDate){
+        List<ExamModel> examsByDate = examRepository.getExamsByDate(examDate);
+        if (examsByDate.isEmpty()){
+            throw new ApiException("No exams found for Date: "+ examDate, HttpStatus.NOT_FOUND);
+        }
+        List<ExamDTO> examDTOS = new ArrayList<>();
+        for (ExamModel exam: examsByDate){
+            examDTOS.add(ExamDTO.fromModel(exam));
+        }
+        return examDTOS;
+
+    }
+    public List<ExamDTO> getExamsByTimeSlot(LocalTime examStartTime, LocalTime examEndTime){
+
+
+            List<ExamModel> examsByTimeSlot = examRepository.getExamsByTimeSlot(examStartTime, examEndTime);
+
+        if (examsByTimeSlot.isEmpty()){
+            throw new ApiException("No exams found for time slot: "+ examStartTime + " / " + examEndTime , HttpStatus.NOT_FOUND);
+        }
+        List<ExamDTO> examDTOS = new ArrayList<>();
+        for (ExamModel exam: examsByTimeSlot){
+            examDTOS.add(ExamDTO.fromModel(exam));
+        }
+        return examDTOS;
     }
 
 }
