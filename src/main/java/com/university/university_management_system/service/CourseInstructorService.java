fb019c4fdee52f14506fca78dae5f07b54d6779c
Maheen Fatima <s.maheenfatima16@gmail.com>
2024-12-16 11:16:48 +0500

    added 2 repositories to show invalid id error

--- src/main/java/com/university/university_management_system/service/CourseInstructorService.java
+++ src/main/java/com/university/university_management_system/service/CourseInstructorService.java
@@ -4,7 +4,10 @@ import com.university.university_management_system.DTOs.CourseInstructorDTO;
 import com.university.university_management_system.exceptions.ApiException;
 import com.university.university_management_system.model.CourseInstructorModel;
 import com.university.university_management_system.model.CourseModel;
+import com.university.university_management_system.model.FacultyModel;
 import com.university.university_management_system.repository.CourseInstructorRepository;
+import com.university.university_management_system.repository.CourseRepository;
+import com.university.university_management_system.repository.FacultyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -17,6 +20,8 @@ public class CourseInstructorService {
 
     @Autowired
     CourseInstructorRepository courseInstructorRepository;
+    @Autowired
+    CourseRepository courseRepository;
 
     public List<CourseInstructorDTO> getAllCourseInstructors(){
         List<CourseInstructorModel> allCourseInstructors = courseInstructorRepository.findAll();
@@ -40,11 +45,10 @@ public class CourseInstructorService {
     public List<CourseInstructorDTO> getCourseInstructorByCourseCode(String courseCode){
         List<CourseInstructorModel> allCourseInstructors = courseInstructorRepository.findAll();
         List<CourseInstructorDTO> courseInstructorsByCourseCode = new ArrayList<>();
-
+        CourseModel course = courseRepository.findById(courseCode).orElseThrow(()->new ApiException("Invalid Course Code", HttpStatus.NOT_FOUND));
         if (allCourseInstructors.isEmpty()){
             throw new ApiException("No Course Instructors", HttpStatus.NOT_FOUND);
         }
-
 
         for(CourseInstructorModel cim : allCourseInstructors){
             if(courseCode.equals(cim.getCourse().getCourseCode())){
@@ -53,8 +57,6 @@ public class CourseInstructorService {
         }
         return courseInstructorsByCourseCode;
     }
-
-
 
     public List <CourseModel> getCoursesByInstructorId(int instructorId){
         List<CourseInstructorModel> allCourseInstructors = courseInstructorRepository.findAll();
