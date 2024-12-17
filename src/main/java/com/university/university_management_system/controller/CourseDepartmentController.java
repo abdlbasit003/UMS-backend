5ed8afb70f4a7d1613d9a0654e7950c015d2498c
QuantumCoderX7 <rayyanasim19@gmail.com>
2024-12-17 10:42:05 +0500

    UMS-178
    Department/Course service & DTO
    UMS-178
    2nd Update
    rayyan

--- src/main/java/com/university/university_management_system/controller/CourseDepartmentController.java
+++ src/main/java/com/university/university_management_system/controller/CourseDepartmentController.java
@@ -1,28 +1,42 @@
 package com.university.university_management_system.controller;
 
-import com.university.university_management_system.model.DepartmentCourseModel;
-import com.university.university_management_system.repository.DepartmentCourseRepository;
+import com.university.university_management_system.DTOs.CourseDepartmentDTO;
+import com.university.university_management_system.service.CourseDepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/coursedepartments")
 public class CourseDepartmentController {
-    @Autowired
-    DepartmentCourseRepository departmentCourseRepository;
-    @GetMapping("")
-    public List<DepartmentCourseModel> getAll(){
-        return  departmentCourseRepository.findAll();
-    }
-    @GetMapping("/{courseDepartmentId}")
-    public DepartmentCourseModel getbyid(@PathVariable int courseDepartmentId){
-        return departmentCourseRepository.findById(courseDepartmentId).orElseThrow();
-    }
-    
 
+    @Autowired
+    public CourseDepartmentService courseDepartmentService;
+
+    @GetMapping("")
+    public ResponseEntity<?> getAllCourseDepartments() {
+        return ResponseEntity.ok().body(courseDepartmentService.getAllDepartmentCourses());
+    }
+
+    @GetMapping("/{courseDepartmentId}")
+    public ResponseEntity<?> getCourseDepartmentById(@PathVariable Integer courseDepartmentId) {
+        return ResponseEntity.ok().body(courseDepartmentService.getDepartmentCourseById(courseDepartmentId));
+    }
+
+    @GetMapping("/department/{departmentId}")
+    public ResponseEntity<?> getCoursesByDepartmentId(@PathVariable Integer departmentId) {
+        return ResponseEntity.ok().body(courseDepartmentService.getCoursesByDepartmentId(departmentId));
+    }
+
+    @GetMapping("/course/{courseCode}")
+    public ResponseEntity<?> getDepartmentsByCourseCode(@PathVariable String courseCode) {
+        return ResponseEntity.ok().body(courseDepartmentService.getDepartmentsByCourseCode(courseCode));
+    }
+
+    @GetMapping("/coursesInDepartment/{departmentName}")
+    public ResponseEntity<?> getAllCoursesInDepartment(@PathVariable String departmentName) {
+        return ResponseEntity.ok().body(courseDepartmentService.getAllCoursesInDepartment(departmentName));
+    }
 }
