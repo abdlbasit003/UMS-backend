6dc70723143d3c27060271a533690904ff981b63
Abdul Basit <basitirfan760@gmail.com>
2024-12-19 04:40:15 +0500

    REST mappings for question and exam questions controller

--- src/main/java/com/university/university_management_system/controller/ExamQuestionsController.java
+++ src/main/java/com/university/university_management_system/controller/ExamQuestionsController.java
@@ -5,15 +5,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/examquestions")
+@RequestMapping("/exams/{examId}/questions")
 public class ExamQuestionsController {
 
     @Autowired
     public ExamQuestionService examQuestionsService;
 
     @GetMapping("")
-    public ResponseEntity<?> getAllExamQuestions() {
-        return ResponseEntity.ok().body(examQuestionsService.getAllExamQuestions());
+    public ResponseEntity<?> getAllExamQuestions(@PathVariable int examId) {
+        return ResponseEntity.ok().body(examQuestionsService.getQuestionsByExamId(examId));
     }
 
     @GetMapping("/{examQuestionId}")
@@ -21,12 +21,12 @@ public class ExamQuestionsController {
         return ResponseEntity.ok().body(examQuestionsService.getExamQuestionById(examQuestionId));
     }
 
-    @GetMapping("/exam/{examId}")
+
     public ResponseEntity<?> getQuestionsByExamId(@PathVariable Integer examId) {
         return ResponseEntity.ok().body(examQuestionsService.getQuestionsByExamId(examId));
     }
 
-    @GetMapping("/question/{questionId}")
+
     public ResponseEntity<?> getExamsByQuestionId(@PathVariable Integer questionId) {
         return ResponseEntity.ok().body(examQuestionsService.getExamsByQuestionId(questionId));
     }
--- src/main/java/com/university/university_management_system/controller/QuestionController.java
+++ src/main/java/com/university/university_management_system/controller/QuestionController.java
@@ -3,24 +3,37 @@ package com.university.university_management_system.controller;
 import com.university.university_management_system.DTOs.QuestionDTO;
 import com.university.university_management_system.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
+@RequestMapping("/questions")
 public class QuestionController {
+
     @Autowired
     QuestionService questionService;
+
+    @GetMapping("")
     public List<QuestionDTO> getAllQuestions(){
         return questionService.getAllQuestions();
     }
 
-
-    public QuestionDTO getQuestionById(int id) {
-        return questionService.getQuestionById(id);
+    @GetMapping("/{questionId}")
+    public QuestionDTO getQuestionById(@PathVariable int questionId) {
+        return questionService.getQuestionById(questionId);
     }
-    public List<QuestionDTO> getQuestionsByTypeId(int questionTypeId) {
+
+    @GetMapping("/type/{questionTypeId}")
+    public List<QuestionDTO> getQuestionsByTypeId(@PathVariable int questionTypeId) {
         return questionService.getQuestionsByTypeId(questionTypeId);
     }
-    public List<QuestionDTO> getQuestionsByCloId(int cloId) {
+
+
+    public List<QuestionDTO> getQuestionsByCloId(@PathVariable int cloId) {
         return questionService.getQuestionsByCloId(cloId);
     }
 
