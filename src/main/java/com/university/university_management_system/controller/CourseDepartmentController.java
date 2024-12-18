f5e0c6b7aa50e26609a78d205c1a49ecb169cb05
Abdul Basit <abdlbasit003@gmail.com>
2024-12-18 23:32:40 +0500

    mappings for Course department controller

--- src/main/java/com/university/university_management_system/controller/CourseDepartmentController.java
+++ src/main/java/com/university/university_management_system/controller/CourseDepartmentController.java
@@ -9,34 +9,28 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/coursedepartments")
 public class CourseDepartmentController {
 
     @Autowired
     public CourseDepartmentService courseDepartmentService;
 
-    @GetMapping("")
-    public ResponseEntity<?> getAllCourseDepartments() {
-        return ResponseEntity.ok().body(courseDepartmentService.getAllDepartmentCourses());
+    @GetMapping("/departments/{departmentId}/courses")
+    public ResponseEntity<?> getAllCourseDepartments(@PathVariable int departmentId) {
+        return ResponseEntity.ok().body(courseDepartmentService.getCoursesByDepartmentId(departmentId));
     }
 
-    @GetMapping("/{courseDepartmentId}")
+    @GetMapping("/departments/{departmentId}/courses/{courseDepartmentId}")
     public ResponseEntity<?> getCourseDepartmentById(@PathVariable Integer courseDepartmentId) {
         return ResponseEntity.ok().body(courseDepartmentService.getDepartmentCourseById(courseDepartmentId));
     }
 
-    @GetMapping("/department/{departmentId}")
-    public ResponseEntity<?> getCoursesByDepartmentId(@PathVariable Integer departmentId) {
-        return ResponseEntity.ok().body(courseDepartmentService.getCoursesByDepartmentId(departmentId));
-    }
-
-    @GetMapping("/course/{courseCode}")
+    @GetMapping("/course/{courseCode}/departments")
     public ResponseEntity<?> getDepartmentsByCourseCode(@PathVariable String courseCode) {
         return ResponseEntity.ok().body(courseDepartmentService.getDepartmentsByCourseCode(courseCode));
     }
 
-    @GetMapping("/coursesInDepartment/{departmentName}")
-    public ResponseEntity<?> getAllCoursesInDepartment(@PathVariable String departmentName) {
-        return ResponseEntity.ok().body(courseDepartmentService.getAllCoursesInDepartment(departmentName));
-    }
+//    @GetMapping("/coursesInDepartment/{departmentName}")
+//    public ResponseEntity<?> getAllCoursesInDepartment(@PathVariable String departmentName) {
+//        return ResponseEntity.ok().body(courseDepartmentService.getAllCoursesInDepartment(departmentName));
+//    }
 }
