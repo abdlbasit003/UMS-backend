6abfeb42144723b9b2daf7a479d9ef0f19c01ff2
Abdullah Imran <imranabdullah958@gmail.com>
2025-01-27 20:35:47 +0500

    added pathvariable,request param and request mapping
    Updated UMB-35

--- src/main/java/com/university/university_management_system/controller/ExamScheduleController.java
+++ src/main/java/com/university/university_management_system/controller/ExamScheduleController.java
@@ -3,60 +3,66 @@ package com.university.university_management_system.controller;
 import com.university.university_management_system.DTOs.ExamScheduleDTO;
 import com.university.university_management_system.service.ExamScheduleService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 
+@RestController
+@RequestMapping("/exam-schedules")
 public class ExamScheduleController {
+
     @Autowired
     private ExamScheduleService examScheduleService;
 
-    @GetMapping("/exam-schedules")
-    public List<ExamScheduleDTO> getAll(){
+    @GetMapping
+    public List<ExamScheduleDTO> getAll() {
         return examScheduleService.getAll();
     }
 
     @GetMapping("/{scheduleId}")
-    public ExamScheduleDTO getByScheduleId(int scheduleId){
+    public ExamScheduleDTO getByScheduleId(@PathVariable int scheduleId) {
         return examScheduleService.getByScheduleId(scheduleId);
     }
 
     @GetMapping("/exam/{examId}")
-    public List<ExamScheduleDTO> getByExamId(int examId){
+    public List<ExamScheduleDTO> getByExamId(@PathVariable int examId) {
         return examScheduleService.getByExamId(examId);
     }
 
     @GetMapping("/room/{roomId}")
-    public List<ExamScheduleDTO>  getByExaminationRoomId(int roomId){
+    public List<ExamScheduleDTO> getByExaminationRoomId(@PathVariable int roomId) {
         return examScheduleService.getByExaminationRoomId(roomId);
     }
 
     @GetMapping("/start-time")
-    public List<ExamScheduleDTO> getByStartTime(LocalTime startTime){
+    public List<ExamScheduleDTO> getByStartTime(@RequestParam LocalTime startTime) {
         return examScheduleService.getByStartTime(startTime);
     }
+
     @GetMapping("/end-time")
-    public List<ExamScheduleDTO> getByEndTime(LocalTime endTime){
+    public List<ExamScheduleDTO> getByEndTime(@RequestParam LocalTime endTime) {
         return examScheduleService.getByEndTime(endTime);
     }
 
     @GetMapping("/date")
-    public List<ExamScheduleDTO> getByExamDate(LocalDate examDate){
+    public List<ExamScheduleDTO> getByExamDate(@RequestParam LocalDate examDate) {
         return examScheduleService.getByExamDate(examDate);
     }
+
     @GetMapping("/department/{departmentId}")
-    public List<ExamScheduleDTO> getCompleteScheduleOfDepartment(int departmentId){
+    public List<ExamScheduleDTO> getCompleteScheduleOfDepartment(@PathVariable int departmentId) {
         return examScheduleService.getCompleteScheduleOfDepartment(departmentId);
     }
+
     @GetMapping("/student/{studentId}")
-    public List<ExamScheduleDTO> getCompleteScheduleOfStudent(String studentId){
+    public List<ExamScheduleDTO> getCompleteScheduleOfStudent(@PathVariable String studentId) {
         return examScheduleService.getCompleteScheduleOfStudent(studentId);
     }
+
     @GetMapping("/course")
-    public List<ExamScheduleDTO> getByCourse(String courseCode){
+    public List<ExamScheduleDTO> getByCourse(@RequestParam String courseCode) {
         return examScheduleService.getByCourse(courseCode);
     }
-
 }
